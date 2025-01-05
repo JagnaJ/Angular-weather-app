8f8daa2fd2943573de96f5d7480d2c8686cca693
JagnaJ <janaponomarenk@gmail.com>
2025-01-06 00:06:01 +0100

    Fix GitHub Pages deployment by moving index.html

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -11,9 +11,9 @@ export default {
   }
 ],
   assets: {
-    'index.csr.html': {size: 622, hash: 'dea220edf5b22c369f197bb2b0896e16f0836a111a6e689d83c33c0ccd654b29', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1024, hash: '360f949514fe3b7e96deda350e3a64f259ee9cb8344c16f1554ac634b37a8bff', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 4897, hash: '3b20e71911517e5ee3c1ee3405670b1478ed8bbc20100680ccac6e3e4f3a036d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 622, hash: '87c65d479c44f66c798aca6aa073e23606452563ac90c966d92fbfc36f2027a5', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1024, hash: '2d4909942205f893109dfd26abf0dc3403432069a2f6f13cd72c70a341fea89d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 5837, hash: '2084729499b676d0303262a0be2c12d88fb597716247a12077be2d96b7303444', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-ZAYVYYH6.css': {size: 11832, hash: 'pqKc/PAC1U8', text: () => import('./assets-chunks/styles-ZAYVYYH6_css.mjs').then(m => m.default)}
   },
 };
